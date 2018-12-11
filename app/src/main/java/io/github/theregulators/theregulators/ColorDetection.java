b58369887594dd28e2781aa13a6f7382fcef1190
Jonathan Lam <jlam55555@gmail.com>
2018-12-11 18:06:11 -0500

    fixed bug with center condition

--- app/src/main/java/io/github/theregulators/theregulators/ColorDetection.java
+++ app/src/main/java/io/github/theregulators/theregulators/ColorDetection.java
@@ -106,7 +106,7 @@ public class ColorDetection {
           xAvg /= currentPixelCluster.pixelCount;
           yAvg /= currentPixelCluster.pixelCount;
 
-          if(Math.abs(xAvg - width/2) < width/4 || Math.abs(yAvg - height/2) > height/4) {
+          if(Math.abs(xAvg - width/2) > width/4 || Math.abs(yAvg - height/2) > height/4) {
             // FILTER CONDITION 2
             continue;
           }
@@ -130,56 +130,5 @@ public class ColorDetection {
     }
 
     return closestColor;
-
-    // scale image
-    // very rough -- just excludes many pixels
-    /*final int maxDim = 100;
-    int scaledWidth, scaledHeight, ratio;
-    if(width >= height) {
-      ratio = width / maxDim;
-      scaledWidth = maxDim;
-      scaledHeight = height * maxDim / width;
-    } else {
-      ratio = height / maxDim;
-      scaledWidth = width * maxDim / height;
-      scaledHeight = maxDim;
-    }
-
-    int scaledBitmap[] = new int[scaledWidth * scaledHeight];
-    for(int i = 0; i < ratio * scaledHeight; i += ratio) {
-      for(int j = 0; j < ratio * scaledWidth; j += ratio) {
-        scaledBitmap[(i / ratio * maxDim + j / ratio)] = bitmap[i * width + j * ratio];
-      }
-    }
-
-    // get average of four corners
-    VectorRGB corner1 = new VectorRGB(scaledBitmap[0]);
-    VectorRGB corner2 = new VectorRGB(scaledBitmap[scaledWidth - 1]);
-    VectorRGB corner3 = new VectorRGB(scaledBitmap[scaledWidth * (scaledHeight - 1)]);
-    VectorRGB corner4 = new VectorRGB(scaledBitmap[scaledBitmap.length - 1]);
-    VectorRGB cornerAverage = corner1.add(corner2).add(corner3).add(corner4).timesScalar(0.25);
-
-    // get off-background pixels
-    final int THRESHOLD = 100;
-    int significantColorCount = 0;
-    VectorRGB significantColorSum = new VectorRGB(0, 0, 0);
-    for(int i = 0; i < scaledBitmap.length; i++) {
-      VectorRGB currentColor = new VectorRGB(scaledBitmap[i]);
-
-      if(currentColor.distanceTo(cornerAverage) > THRESHOLD) {
-        significantColorSum = significantColorSum.add(currentColor);
-        significantColorCount++;
-      }
-    }
-    if(significantColorCount == 0) {
-      System.out.println("No significant points.");
-      return new VectorRGB(0);
-    }
-    VectorRGB significantColorAverage = significantColorSum.timesScalar(1.0/significantColorCount);
-
-    System.out.println("SUM: " + significantColorSum.toString() + " COUNT: " + significantColorCount + "AVERAGE COLOR: " + significantColorAverage.toString());
-
-    return significantColorAverage;*/
   }
-
 }
