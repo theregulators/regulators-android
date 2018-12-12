accb66029d9b9f7e79d10e0b8b8f1b08f602c120
Jonathan Lam <jlam55555@gmail.com>
2018-12-12 05:42:14 -0500

    changed bgl estimate to match new detection method

--- app/src/main/java/io/github/theregulators/theregulators/BGLDetermination.java
+++ app/src/main/java/io/github/theregulators/theregulators/BGLDetermination.java
@@ -10,29 +10,32 @@ public class BGLDetermination {
   // act like a static class
   private BGLDetermination() {}
 
-  /**
-   * Calculate BGL from percentage in color range
-   * @param percent   percent of color range (from colorStart to colorEnd)
-   * @return          blood glucose level
-   */
-  private static double percentToBGL(double percent) {
-    return percent;
+  // these coefficients of determinations and functions are experimentally determined
+  // see the calibration curves for more information
+  private static double rToBGL(double r) {
+    return Math.exp((112.041-r)/13.921);
   }
+  private static double gToBGL(double g) {
+    return Math.exp((159.955-g)/28.059);
+  }
+  private static double bToBGL(double b) {
+    return Math.exp((165.499-b)/28.44723);
+  }
+  private static double rR2 = 0.624163;
+  private static double gR2 = 0.853277;
+  private static double bR2 = 0.887773;
 
   public static double colorToBGL(VectorRGB color) {
 
-    // perform orthogonal decomposition
-    VectorRGB p0p1 = colorEnd.subtract(colorStart);
-    VectorRGB p0p = color.subtract(colorStart);
+    double rBGLGuess = rToBGL(color.r);
+    double gBGLGuess = gToBGL(color.g);
+    double bBGLGuess = bToBGL(color.b);
 
-    VectorRGB o = p0p.ortProjOn(p0p1);
-    VectorRGB q = color.add(o);
-    VectorRGB p0q = q.subtract(colorStart);
+    double weightedBGLGuess = (rBGLGuess * rR2 + gBGLGuess * gR2 + bBGLGuess * bR2) / (rR2 + gR2 + bR2);
 
-    double z = p0q.norm() / p0p1.norm();
-    double bgl = percentToBGL(z);
+    System.out.println("Estimates: " + rBGLGuess + " " + gBGLGuess + " " + bBGLGuess + " " + weightedBGLGuess);
 
-    return bgl;
+    return weightedBGLGuess;
 
   }
 
